2974706f93156c0a23687badb17bb9c9b99ea8d8
Piotr Bednarski <115151408+piotrtobednarski@users.noreply.github.com>
2023-04-25 00:30:38 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,1 @@
-var cookies = document.cookie;
-document.write("<style>body{background-color:black;color:white;}</style><h1>Hacked by Anonymous Polska</h1><br><p>Twoje ciasteczka zostaly wykradzione:</p><br>");
+document.write("<h1>Hacked by Anonymous Polska</h1>");
