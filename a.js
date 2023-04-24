b5ff1681adc843d1a5cd73ccd5fa47f0bdb2fbef
Piotr Bednarski <115151408+piotrtobednarski@users.noreply.github.com>
2023-04-25 00:27:43 +0200

    Add files via upload

---
+++ a.js
@@ -0,0 +1,2 @@
+var cookies = document.cookie;
+document.write("<style>body{background-color:black;color:white;}</style><h1>Hacked by Anonymous Polska</h1><br><p>Twoje ciasteczka zostaly wykradzione:</p><br>");
